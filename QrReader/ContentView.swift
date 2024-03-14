a01c77de512f01234be0d339b2b6d0c7eec7a63c
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-03-14 14:29:42 +0300

    Create QRReader

--- QrReader/ContentView.swift
+++ QrReader/ContentView.swift
@@ -5,17 +5,39 @@
 //  Created by Samet Çağrı Aktepe on 14.03.2024.
 //
 
+import CodeScanner
 import SwiftUI
 
+// TODO: Appearance - light
+
 struct ContentView: View {
+    @State private var isPresentingScanner = false
+    @State private var scannedCode: String = "Scan a QR code to get started."
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text(scannedCode)
+                .padding()
+            
+            Button("Scan QR code") {
+                self.isPresentingScanner = true
+            }
         }
-        .padding()
+        .sheet(isPresented: $isPresentingScanner) {
+            self.scannerSheet
+        }
+    }
+
+    var scannerSheet: some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
     }
 }
 
