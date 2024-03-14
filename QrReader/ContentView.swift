b6e1882d376a7c8285f455c1c9b181ee866d360a
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-03-14 14:07:36 +0300

    Initial Commit

---
+++ QrReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QrReader
+//
+//  Created by Samet Çağrı Aktepe on 14.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QrReader/QrReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrReaderApp.swift
+//  QrReader
+//
+//  Created by Samet Çağrı Aktepe on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct QrReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
